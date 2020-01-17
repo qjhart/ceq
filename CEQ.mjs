7f37ebca7e9ec41cd6b59d9f27a9ad9d476f195b
Quinn Hart <qjhart@ucdavs.edu>
2020-01-17 01:46:08 -0800

    added center_to_ceq.

--- CEQ.mjs
+++ CEQ.mjs
@@ -5,47 +5,75 @@ export class CEQ {
 				let re = /^(.*[:/])?(([7EF])|([0123])([0-9,A-F]*)(\.([0-3]*))?)$/i;
 				return ceq.match(re);
 		}
-		// Also a constructor, should be same as below
-  static fromNE(ne,level) {
-    let n=10;
-    let prefix=0;
-		let center_dis=2**n*1000;    // nth level distance to center
-    let rat=[ne[0]/center_dis,ne[1]/center_dis];
-    if (Math.abs(rat[0]) > 1 || Math.abs(rat[1]) > 1)
-      return('q:F');
-    if (rat[0]>0)
-      prefix |= 0x2;
-    if (rat[1]>0)
-      prefix |= 0x1;
-
-    console.log(`prefix:${prefix},rat=${rat},n=${n},center_dis=${center_dis}`);
-
-    return('q:E');
+	// Also a constructor, should be same as below
+  static b4_to_hex(b4) {
+    let hex='';
+		if (b4 && b4.length > 1) {
+			for(let b=0; b<b4.length-1; b+=2) {
+				let h=parseInt(b4.charAt(b),8);
+				let l=parseInt(b4.charAt(b+1),8);
+				let x=(h << 2 | l).toString(16).toUpperCase();
+				console.log('b:'+b+' x:'+x);
+				hex+=x;
+				b4=b4.slice(2);
+			}
+		}
+    return[hex,b4];
   }
 
+  static center_to_ceq(c) {
+    let n_max=11;
+    let n;
+    let prefix=0;
 
-  static foo(cnl) {
-		console.log('foo:'+typeof cnl);
-		let level=cnl[1] || 10;
-		let center_dis=2**level*1000;
-		let lr=[cnl[2][0],cnl[2][1]];
+    if (c[0]===0 && c[1]===0)
+      return('7');
 
-		let b4='';
-		for (let i=level; i<=10;i++) {
-			console.log(`i:${i} b4:${b4} lr:${lr}`);
-			b4=((lr[0] & 0x1 ) << 1 | (lr[1] & 0x1)).toString(4)+b4;
-			lr[0]>>=1;
-			lr[1]>>=1;
+    if (Math.sign(c[1])==-1)
+      prefix |=0x2;
+    if (Math.sign(c[0])==-1)
+      prefix |=0x1;
+
+    let n_min=0;
+    let min=2**n_min*1000;
+    let i=[Math.abs(Math.round(c[0]/min)),Math.abs(Math.round(c[1]/min))];
+
+    if (Math.sign(c[0])*i[0]*min != c[0] || Math.sign(c[1])*i[1]*min != c[1])
+      return('E');
+
+    n=n_max-n_min-1;
+
+    // Check if out of CA, ie bigger than 2^10
+    let rat=[i[0]>>n,i[1]>>n];
+    let rat_t=(rat[0] || rat[1]);
+
+    console.log(`n=${n},c=${c},i=${i} rat=${rat} rat_t=${rat_t}`);
+
+    if (rat_t)
+      return('F');
+
+    let b4='';
+    for (--n; n>0; n--) {
+      let d=((i[0]>>n ^ 0x1) << 1 | (i[1]>>n ^ 0x1)).toString(4);
+      i=[i[0] & ~(0x1<<n) ,i[1] & ~(0x1<<n)];
+      if ((i[0] | i[1]) == 0) // You have reached the size
+        break;
+			b4=b4+d;
+      console.log(`n=${n},i=${i},2^n=${2**n},d=${d},b4=${b4}`);
+    }
+		let ceq=prefix;
+		if (b4) {
+      let hex;
+      [hex,b4] = this.b4_to_hex(b4);
+			ceq+=hex;
+      if (b4)
+        ceq+='.'+b4;
 		}
-		console.log('b4: '+b4,'lr:'+lr);
-		if (lr) {
-			console.log('center:'+(cnl[2][0]+0.5)*center_dis+','+(cnl[2][1]+0.5)*center_dis)
-		}
-
-	}
+    return ceq;
+  }
 
   constructor(ceq) {
-		console.log('typeof:'+typeof ceq);
+  console.log('typeof:'+typeof ceq);
 		if (typeof ceq == 'object') {
       // Later we can check combine from NE above
 			ceq='q:E';
@@ -78,6 +106,7 @@ export class CEQ {
 			this.ceq+='.'+this.b4;
 		}
   }
+
 	center_level() {
 		console.log('center_level:',this.prefix);
 		let n=10;               // level = pixel width
@@ -132,8 +161,4 @@ export class CEQ {
 	lr() {
 		return (this.center_level())[2];
 	}
-
-	endpoints() {
-	}
-
 }
